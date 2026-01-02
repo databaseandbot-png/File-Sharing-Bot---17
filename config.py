f22ff11e7a810801dd84093004f01f22a89bc4c3
Davi Botz <databaseandbot@gmail.com>
2026-01-02 21:14:04 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,111 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "25237912"))
+API_HASH = os.environ.get("API_HASH", "e192dcc0926b4f447b8f28187f7e533b")
+
+
+OWNER_ID = int(os.environ.get("OWNER_ID", "5751033456"))
+DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "Tom")
+
+
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003136096998"))
+
+FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1003125658097"))
+
+FORCE_SUB_CHANNEL_2 = int(os.environ.get("FORCE_SUB_CHANNEL_2", "-1003192857653"))
+
+FORCE_SUB_CHANNEL_3 = int(os.environ.get("FORCE_SUB_CHANNEL_3", "0"))
+
+FORCE_SUB_CHANNEL_4 = int(os.environ.get("FORCE_SUB_CHANNEL_4", "0"))
+
+START_PIC = os.environ.get("START_PIC", "https://files.catbox.moe/ee65fy.jpg")
+F_PIC = os.environ.get("FORCE_PIC", "https://files.catbox.moe/3c40l3.jpg")
+
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "86400")) # auto delete in seconds
+
+
+PORT = os.environ.get("PORT", "8050")
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+
+
+try:
+    ADMINS=[5751033456 ,8581819072]
+    for x in (os.environ.get("ADMINS", "5751033456").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+
+
+
+
+
+
+
+
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+
+DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', None) == "True" else False
+
+BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
+
+
+
+
+
+
+
+USER_REPLY_TEXT = "❌Sry You can't Able to Message me !\n\n» My Owner 👉 "
+
+START_MSG = os.environ.get("START_MESSAGE", "<b>Hi {first} Friend I am a Advance File Store bot 😈 \n\n I was created by 👉@orewa_luffy72 </b>")
+
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "𝐒𝐨𝐫𝐫𝐲 {first} You must join the given channels ..\n\n 𝐒𝐨 please join and  “𝐍𝐨𝐰 𝐂𝐥𝐢𝐜𝐤 𝐡𝐞𝐫𝐞” 𝐛𝐮𝐭𝐭𝐨𝐧....!")
+
+
+
+
+ADMINS.append(5751033456)
+ADMINS.append(8581819072)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+   
+
+class Txt(object):
+    about = f"""<b>😈 My Name :</b> <a href=''>[AW] File store bot 😈 </a>
+<b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
+<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
+<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
+<b>📢 Channel :</b> <a href='https://t.me/orewa_void'>Tom Botz</a>
+<b>🛡️ :</b> <a href='https://t.me/orewa_gaban'>Davi Developer</a>
+    
+<b>😈 Bot Made By :</b> @orewa_luffy72"""
+  
